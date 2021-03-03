a2f2b614e918030370b262268c7404b19c5161da
Ishan Singh <ishan423@gmail.com>
2021-03-03 13:07:16 -0500

    final changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Ishan Singh</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
