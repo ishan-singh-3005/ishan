8589b44e07245eab8f7b40940c5b4b578918acb3
ishan-singh-3005 <ishan423@gmail.com>
2023-08-15 22:58:41 -0400

    updated resume data

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -25,7 +25,9 @@ class Resume extends Component {
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="info">{work.years}</em></p>
-            <p>{work.description}</p>
+            {work.description.map((item, index) => (
+              <li>{item}</li>
+            ))}
         </div>
       })
 
